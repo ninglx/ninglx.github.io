f0a06c0581ed351e8dad3ec2b949abe91a0691e3
ninglx <ninglixin@wanji.net.cn>
2025-07-07 17:37:26 +0800

    'update'

--- demos/map/cesium/cesiumInit.js
+++ demos/map/cesium/cesiumInit.js
@@ -42,92 +42,157 @@ viewer.scene.screenSpaceCameraController.rotateEventTypes = [
 viewer.scene.globe.enableLighting = true;
 viewer.scene.postProcessStages.fxaa.enabled = true
 
-let entities = {} // 用来保存entities
-let canvas = viewer.scene.canvas;
-let handler = new Cesium.ScreenSpaceEventHandler(canvas);
-handler.setInputAction(function (lclickment) {
-    let scene = viewer.scene;
-    let ellipsoid = scene.globe.ellipsoid;
-    //let cartesian = LoadCesium.Viewer.camera.pickEllipsoid(lclickment.position, ellipsoid);
-    let cartesian = viewer.scene.pickPosition(lclickment.position);
-    positionPick = cartesian;
-    let pinBuilder = new Cesium.PinBuilder();
-    if (cartesian) {
-        let cartographic = ellipsoid.cartesianToCartographic(cartesian);
-        lon = Cesium.Math.toDegrees(cartographic.longitude).toFixed(7);
-        lat = Cesium.Math.toDegrees(cartographic.latitude).toFixed(7);
-        //地理高度
-        height1 = (cartographic.height + 1).toFixed(2);
-        //相机高度
-        height = viewer.camera.positionCartographic.height.toFixed(0);
-        //方向   围绕Z轴旋转
-        heading = Cesium.Math.toDegrees(viewer.camera.heading).toFixed(2);
-        //倾斜角度   围绕Y轴旋转
-        pitch = Cesium.Math.toDegrees(viewer.camera.pitch).toFixed(2);
-        //围绕X轴旋转
-        roll = Cesium.Math.toDegrees(viewer.camera.roll).toFixed(2);
-        console.log(`[${lon},${lat},${height1}]`);
-        console.log('heading:' + heading + ';' + 'pitch:' + pitch + ';' + 'roll:' + roll)
+// 流动线条start
+// 定义线条的起点和终点
+var startPoint = Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883);
+var endPoint = Cesium.Cartesian3.fromDegrees(-75.53777, 40.08883);
+
+// 自定义材质
+var customMaterial = new Cesium.Material({
+    fabric: {
+        type: 'CustomArrowFlow',
+        uniforms: {
+            time: 0,
+            arrowSize: 0.1,
+            arrowGap: 0.2,
+            arrowColor: new Cesium.Color(1.0, 0.0, 0.0, 1.0),
+            lineColor: new Cesium.Color(1.0, 1.0, 1.0, 1.0)
+        },
+        source: `
+                    uniform float time;
+                    uniform float arrowSize;
+                    uniform float arrowGap;
+                    uniform vec4 arrowColor;
+                    uniform vec4 lineColor;
+
+                    czm_material czm_getMaterial(czm_materialInput materialInput) {
+                        czm_material material = czm_getDefaultMaterial(materialInput);
+                        vec2 st = materialInput.st;
+                        float distance = st.x;
+                        float phase = fract(distance / arrowGap + time);
+                        if (phase < arrowSize) {
+                            material.diffuse = arrowColor.rgb;
+                            material.alpha = arrowColor.a;
+                        } else {
+                            material.diffuse = lineColor.rgb;
+                            material.alpha = lineColor.a;
+                        }
+                        return material;
+                    }
+                `
+    },
+    translucent: false
+});
+
+// 创建线条实体
+var lineEntity = viewer.entities.add({
+    polyline: {
+        positions: [startPoint, endPoint],
+        width: 10,
+        material: customMaterial,
+        clampToGround: true
     }
-}, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+});
 
-entities.scene = viewer.entities.add({
-    id: `entity001`,
-    model: {
-        uri: './station.glb',
-        scale: 1,
-        // minimumPixelSize: 128,
-        color: Cesium.Color.fromCssColorString('#fff'),
-    },
-})
-entities.scene.position = Cesium.Cartesian3.fromDegrees(
-    133,
-    22,
-    0
-)
-viewer.zoomTo(entities.scene)
-
-let path = [
-    {location: [133.0386878, 22.0361746, 38.12], angle: 262},
-    {location: [133.0385028, 22.0361512, 38.11], angle: 262},
-    {location: [133.0382878, 22.0361145, 38.09], angle: 262},
-    {location: [133.0381455, 22.0360983, 38.11], angle: 262},
-    {location: [133.0378574, 22.0360555, 38.09], angle: 262},
-    {location: [133.0376871, 22.0360262, 38.08], angle: 262},
-    {location: [133.0375448, 22.0360056, 38.07], angle: 262},
-    {location: [133.0373521, 22.0359712, 38.05], angle: 262},
-    {location: [133.0372766, 22.0359540, 38.05], angle: 262},
-    {location: [133.0371469, 22.0359320, 38.03], angle: 262},
-    {location: [133.0370439, 22.0359174, 38.03], angle: 262},
-    {location: [133.0369039, 22.0359008, 38.02], angle: 262},
-]
-
-let carEntity = viewer.entities.add({
-    id: `entity002`,
-    model: {
-        uri: './car.gltf',
-        scale: 1,
-        color: Cesium.Color.fromCssColorString('#fff'),
-    },
-})
-let initT = '2000-01-01 00:00:00.000'
-for (let i = 0; i < path.length; i++) {
-    updateEntityProperty(carEntity, convertDate(initT, i * 4), path[i].location[0], path[i].location[1], path[i].location[2], path[i].angle);
+// 动画循环
+var startTime = Date.now();
+function animate() {
+    var elapsedTime = (Date.now() - startTime) / 1000;
+    customMaterial.uniforms.time = elapsedTime * 0.1;
+    viewer.scene.requestRender();
+    requestAnimationFrame(animate);
 }
-viewer.clock.startTime = Cesium.JulianDate.fromDate(new Date(initT)).clone();
-viewer.clock.currentTime = Cesium.JulianDate.fromDate(new Date(initT)).clone();
+animate();
+viewer.trackedEntity = lineEntity
+// 流动线条end
+
+
+// let entities = {} // 用来保存entities
+// let canvas = viewer.scene.canvas;
+// let handler = new Cesium.ScreenSpaceEventHandler(canvas);
+// handler.setInputAction(function (lclickment) {
+//     let scene = viewer.scene;
+//     let ellipsoid = scene.globe.ellipsoid;
+//     //let cartesian = LoadCesium.Viewer.camera.pickEllipsoid(lclickment.position, ellipsoid);
+//     let cartesian = viewer.scene.pickPosition(lclickment.position);
+//     positionPick = cartesian;
+//     let pinBuilder = new Cesium.PinBuilder();
+//     if (cartesian) {
+//         let cartographic = ellipsoid.cartesianToCartographic(cartesian);
+//         lon = Cesium.Math.toDegrees(cartographic.longitude).toFixed(7);
+//         lat = Cesium.Math.toDegrees(cartographic.latitude).toFixed(7);
+//         //地理高度
+//         height1 = (cartographic.height + 1).toFixed(2);
+//         //相机高度
+//         height = viewer.camera.positionCartographic.height.toFixed(0);
+//         //方向   围绕Z轴旋转
+//         heading = Cesium.Math.toDegrees(viewer.camera.heading).toFixed(2);
+//         //倾斜角度   围绕Y轴旋转
+//         pitch = Cesium.Math.toDegrees(viewer.camera.pitch).toFixed(2);
+//         //围绕X轴旋转
+//         roll = Cesium.Math.toDegrees(viewer.camera.roll).toFixed(2);
+//         console.log(`[${lon},${lat},${height1}]`);
+//         console.log('heading:' + heading + ';' + 'pitch:' + pitch + ';' + 'roll:' + roll)
+//     }
+// }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+//
+// entities.scene = viewer.entities.add({
+//     id: `entity001`,
+//     model: {
+//         uri: './station.glb',
+//         scale: 1,
+//         // minimumPixelSize: 128,
+//         color: Cesium.Color.fromCssColorString('#fff'),
+//     },
+// })
+// entities.scene.position = Cesium.Cartesian3.fromDegrees(
+//     133,
+//     22,
+//     0
+// )
+// viewer.zoomTo(entities.scene)
+//
+// let path = [
+//     {location: [133.0386878, 22.0361746, 38.12], angle: 262},
+//     {location: [133.0385028, 22.0361512, 38.11], angle: 262},
+//     {location: [133.0382878, 22.0361145, 38.09], angle: 262},
+//     {location: [133.0381455, 22.0360983, 38.11], angle: 262},
+//     {location: [133.0378574, 22.0360555, 38.09], angle: 262},
+//     {location: [133.0376871, 22.0360262, 38.08], angle: 262},
+//     {location: [133.0375448, 22.0360056, 38.07], angle: 262},
+//     {location: [133.0373521, 22.0359712, 38.05], angle: 262},
+//     {location: [133.0372766, 22.0359540, 38.05], angle: 262},
+//     {location: [133.0371469, 22.0359320, 38.03], angle: 262},
+//     {location: [133.0370439, 22.0359174, 38.03], angle: 262},
+//     {location: [133.0369039, 22.0359008, 38.02], angle: 262},
+// ]
+//
+// let carEntity = viewer.entities.add({
+//     id: `entity002`,
+//     model: {
+//         uri: './car.gltf',
+//         scale: 1,
+//         color: Cesium.Color.fromCssColorString('#fff'),
+//     },
+// })
+// let initT = '2000-01-01 00:00:00.000'
+// // for (let i = 0; i < path.length; i++) {
+// //     updateEntityProperty(carEntity, convertDate(initT, i * 4), path[i].location[0], path[i].location[1], path[i].location[2], path[i].angle);
+// // }
+// viewer.clock.startTime = Cesium.JulianDate.fromDate(new Date(initT)).clone();
+// viewer.clock.currentTime = Cesium.JulianDate.fromDate(new Date(initT)).clone();
 viewer.clock.shouldAnimate = true
-viewer.clock.onTick.addEventListener(() => {
-    if (viewer.clock.shouldAnimate) {
-        if (viewer.trackedEntity) {
-            setCameraView(viewer.trackedEntity, 43, 12, -10);
-        }
-    }
-})
-setInterval(() => {
-    viewer.clock.currentTime = Cesium.JulianDate.fromDate(new Date(initT)).clone();
-}, 10 * 4 * 1000)
-viewer.trackedEntity = carEntity
+// viewer.clock.onTick.addEventListener(() => {
+//     // if (viewer.clock.shouldAnimate) {
+//     //     if (viewer.trackedEntity) {
+//     //         setCameraView(viewer.trackedEntity, 43, 12, -10);
+//     //     }
+//     // }
+// })
+// setInterval(() => {
+//     viewer.clock.currentTime = Cesium.JulianDate.fromDate(new Date(initT)).clone();
+// }, 10 * 4 * 1000)
+// viewer.trackedEntity = carEntity
 
 // 更新输入entity的property
 function updateEntityProperty(entity, timestamp, lng, lat, height, angle) {
